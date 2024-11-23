fa154811bad61ea48571ad398f18523cca41ddd1
Mateus Gomes <mateusgomes.dev@gmail.com>
2024-11-23 11:44:37 -0300

    criando refresh_token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -68,3 +68,35 @@ def test_token_wrong_username(client, user):
 
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json() == {'detail': 'Incorrect username or password'}
+
+
+def test_refresh_token(client, token):
+    response = client.post(
+        '/auth/refresh_token/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    data = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert data['token_type'] == 'Bearer'
+    assert 'access_token' in data
+    assert 'token_type' in data
+
+
+def test_token_expired_dont_refresh(client, user):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.username, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = client.post(
+            '/auth/refresh_token',
+            headers={'Authorization': f'Bearer {token}'},
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
